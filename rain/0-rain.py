6f6be150f1a23aa2e94a5e9cb14bbf5655f8a61f
Islam-19984 <i.hemada@alustudent.com>
2023-09-27 23:45:31 +0200

    .\0-rain.py

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python3
+
 def rain(walls):
+    """
+    Calculate the amount of rainwater that can be retained between walls.
+
+    Args:
+        walls (list[int]): A list of non-negative integers representing wall heights.
+
+    Returns:
+        int: The total amount of rainwater retained.
+
+    This function calculates the amount of rainwater that can be retained between walls
+    by finding the maximum height of walls to the left and right of each position and
+    determining the water that can be trapped at each position.
+
+    Assumptions:
+    - The ends of the list (before index 0 and after index walls[-1]) are not walls,
+      meaning they will not retain water.
+    - If the list is empty, it returns 0.
+    """
+
     if not walls:
         return 0
 
@@ -22,7 +42,12 @@ def rain(walls):
     return total_water
 
 if __name__ == "__main__":
-    walls = [0, 1, 0, 2, 0, 3, 0, 4]
-    print(rain(walls))  # Output: 6
-    walls = [2, 0, 0, 4, 0, 0, 1, 0]
-    print(rain(walls))  # Output: 6
+    # Test cases
+    print(rain([]))  # Correct output: 0
+    print(rain([2, 0, 2]))  # Correct output: 2
+    print(rain([0, 1, 0, 2, 0, 3, 0, 4]))  # Correct output: 6
+    print(rain([1, 1, 2, 0, 1, 1, 1]))  # Correct output: 0
+    print(rain([0, 2, 1, 0, 1, 3, 1, 2, 1, 1, 2, 1]))  # Correct output: 7
+    print(rain([2, 0, 0, 0, 0, 3, 0]))  # Correct output: 6
+    print(rain([1]))  # Correct output: 0
+    print(rain([3, 3]))  # Correct output: 0
