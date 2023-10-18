a61ee399c02a6f88ef69c75279a54ca872e057dd
Islam-19984 <i.hemada@alustudent.com>
2023-10-18 12:09:26 +0000

    0-pascal_triangle.py

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
+'''A module for working with pascal's triangle.
+'''
+
 
 def pascal_triangle(n):
-    # Check if n is less than or equal to 0
-    if n <= 0:
-        return []
-
-    # Initialize the result list with the first row
-    result = [[1]]
-
-    for i in range(1, n):
-        # Calculate the next row based on the previous row
-        previous_row = result[-1]
-        next_row = [1]
-        for j in range(1, i):
-            next_row.append(previous_row[j - 1] + previous_row[j])
-        next_row.append(1)
-
-        # Append the next row to the result
-        result.append(next_row)
-
-    return result
-
+    '''Creates a lists of integers representing
+    the pascal's triangle of a given integer.
+    '''
+    triangle = []
+    if type(n) is not int or n <= 0:
+        return triange
+    for i in range(n):
+        line = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(triange[i - 1][j - 1] + triange[i - 1][j])
+        triange.append(line)
+    return triangle
